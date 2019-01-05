dc61751a2400132396d98242babb8ab5fb8c11c1
yutamonb <yutamonb@gmail.com>
2019-01-05 17:43:37 +0900

    basic認証追加

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var basicAuth = require('basic-auth-connect');
 
 app.use(morgan('short'));
+app.use(basicAuth('hage', 'hirakegoma'));
+
 app.use('/version', require('./endpoint/version'));
 
 app.listen(8000);
